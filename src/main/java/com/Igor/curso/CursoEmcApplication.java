8e744b448cf2ada221feeaf783965da73085a541
igor thiago <igorthiago255@hotmail.com>
2021-09-03 21:08:57 -0400

    Projeto criado

---
+++ src/main/java/com/Igor/curso/CursoEmcApplication.java
@@ -0,0 +1,14 @@
+package com.Igor.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoEmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoEmcApplication.class, args);
+	}
+
+}
+
